d51d5681b885ee8cd66672731c8ef660a49f82fe
duzhongbo <710409599@qq.com>
2015-11-12 11:09:22 +0800

    添加浮点乘法计算

--- js/common.js
+++ js/common.js
@@ -41,6 +41,29 @@ var common={
 				return '';
 			});
 		return data;
+	},
+	/**
+	 ** 乘法函数，用来得到精确的乘法结果
+	 ** 说明：javascript的乘法结果会有误差，在两个浮点数相乘的时候会比较明显。这个函数返回较为精确的乘法结果。
+	 ** 调用：accMul(arg1,arg2)
+	 ** 返回值：arg1乘以 arg2的精确结果
+	 **/
+	accMul:function(arg1, arg2){
+	    var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+	    try {
+	        m += s1.split(".")[1].length;
+	    }
+	    catch (e) {
+	    }
+	    try {
+	        m += s2.split(".")[1].length;
+	    }
+	    catch (e) {
+	    }
+	    return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);		
 	}
-
 }
+// 给Number类型增加一个mul方法，调用起来更加方便。
+Number.prototype.mul = function (arg) {
+    return common.accMul(arg, this);
+};
